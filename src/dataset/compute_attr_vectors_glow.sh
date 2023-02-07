3a55159083543438b8ee6f54da56ffe73151392c
Tsatsral <124377530+Mametchiii@users.noreply.github.com>
2023-02-07 13:47:04 +0100

    Upload cleaned code

--- src/dataset/compute_attr_vectors_glow.sh
+++ src/dataset/compute_attr_vectors_glow.sh
@@ -18,31 +18,8 @@ case "$dset" in
 celeba64)
   python compute_attr_vectors_glow.py \
     --batch_size 512 --dataset glow_celeba_64_latent_lmdb --image_size 64 \
-    --attributes "Pale_Skin,Young,Blond_Hair,Heavy_Makeup" \
+    --attributes "Pale_Skin,Young,Blond_Hair,Bald,Big_Lips,Chubby,Narrow_Eyes" \
     --gen_model_type Glow --gen_model_name "glow_celeba_64" --glow_n_flow 32 --glow_n_block 4 \
-    --rewrite True \
-    "$@"
-  ;;
-
-celeba64_discover)
-  cd ../standard_classifier
-  ./celeba_64.sh train-cnn --classify_attributes Smiling
-  cd ../dataset
-  python compute_attr_vectors_glow.py \
-    --batch_size 512 --dataset glow_celeba_64_latent_lmdb --image_size 64 \
-    --gen_model_type Glow --gen_model_name "glow_celeba_64" --glow_n_flow 32 --glow_n_block 4 \
-    --rewrite True \
-    --computation_method discover --epochs 3 --lr 0.001 --target Smiling \
-    --fair_classifier_name "std_cls/gen_model=glow_celeba_64/Smiling_resnet_seed_42_epochs_5_batch_size_500_lr_0.001_use_bn_True_pretrained_False" \
-    --encoder_type resnet --encoder_normalize_output False \
-    "$@"
-  ;;
-
-celeba128)
-  python compute_attr_vectors_glow.py \
-    --batch_size 512 --dataset glow_celeba_128_latent_lmdb --image_size 128 \
-    --attributes "Pale_Skin,Young,Blond_Hair,Heavy_Makeup" \
-    --gen_model_type Glow --gen_model_name "glow_celeba_128" --glow_n_flow 32 --glow_n_block 5 \
     --rewrite True \
     "$@"
   ;;
@@ -50,20 +27,12 @@ celeba128)
 fairface)
   python compute_attr_vectors_glow.py \
     --batch_size 512 --dataset glow_fairface_latent_lmdb --image_size 64 --classify_attributes Race \
-    --attributes "Black" \
+    --attributes "Black,Indian" \
     --gen_model_type Glow --gen_model_name "glow_fairface" --glow_n_flow 32 --glow_n_block 4 \
     --rewrite True \
     "$@"
   ;;
 
-3dshapes)
-  python compute_attr_vectors_glow.py \
-    --batch_size 512 --dataset glow_3dshapes_latent_lmdb --image_size 64 \
-    --attributes "orientation" \
-    --gen_model_type Glow --gen_model_name "glow_3dshapes" --glow_n_flow 32 --glow_n_block 3 \
-    --rewrite True \
-    "$@"
-  ;;
 
 *)
   echo "Dataset mode $dset not recognized and thus not supported"
